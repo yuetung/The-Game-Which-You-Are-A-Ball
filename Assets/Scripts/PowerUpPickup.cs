d3d23972d3e08ff1e9347e0f17c7aa47c54b00b1
yuetung <limyuettung@hotmail.com>
2018-02-13 20:40:41 +0800

    Initial Commit
    basic player control (click & drag), basic projectile & power up, with animation

---
+++ Assets/Scripts/PowerUpPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpPickup : MonoBehaviour {
+
+	public PlayerControler.ElementType elementType;
+	[Tooltip("explosion particle effect")]
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Collision with player
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			other.gameObject.GetComponent<PlayerControler> ().gainElementType (elementType);
+			if (explosion) {
+				Instantiate (explosion, transform.position, transform.rotation);
+			}
+			DestroyObject (this.gameObject);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+	
+	[Tooltip("true projectile speed = player base speed * projectile speed")]
+	public float projectileSpeed = 1.0f;
+
+	[Tooltip("true damage = player base power * projectile damage")]
+	public float projectileDamage = 1.0f;
+
+	[Tooltip("how many seconds before destroying object when hit, leave just enought to play animation")]
+	public float explodeAnimationSeconds = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Collision with wall
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Wall") {
+			gameObject.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			gameObject.GetComponent<Animator> ().SetTrigger ("Explode");
+			Invoke ("DestroyNow", explodeAnimationSeconds);
+		}
+	}
+
+	void DestroyNow() {
+		DestroyObject (this.gameObject);
+	}
+}
