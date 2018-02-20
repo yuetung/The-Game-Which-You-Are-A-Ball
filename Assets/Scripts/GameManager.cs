d9250170b39f5babffe0b3f7a0b0387f3aee4922
Yilong94 <yilong_tan@mymail.sutd.edu.sg>
2018-02-20 09:21:27 +0800

    Created UI canvas and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+
+	public Text mainHealthDisplay;
+	public Text mainEnergyDisplay;
+	public Text mainElementDisplay;
+
+	// Use this for initialization
+	void Start () {
+		if (gm == null)
+			gm = this.gameObject.GetComponent<GameManager> ();
+		mainHealthDisplay.text = "Health: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 
+	}
+
+	//create powerup function to be called to update the energy
+
+	//create healthreduced function to be called to update the health bar
+
+	//create powerreduced function to be called to update the energy
+
+	// create elementchange function to be called to update the element
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,6 @@
-Yilong's branch for GUI Design
+To-do list:
+
+- Create game canvas and initialize UI elements [Checked]
+- Create game manager [Checked]
+- Create scripts that update the health, energy and element
+- Create gameover UI and playagain UI
