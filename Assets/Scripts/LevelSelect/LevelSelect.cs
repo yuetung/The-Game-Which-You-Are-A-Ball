a04ec076e0db17d1f4f54f77ed124f098b49507e
KennySoh <32699647+KennySoh@users.noreply.github.com>
2018-03-11 13:03:01 +0800

    Level Select UI & Level Transition From Game won inputs
    Level Select UI bugs fixed,
    -Level unlocks accordingly
    -Option> Resets (Level)
    -Level Progress stored in as in PlayerPrefs.GetInt("LevelReached")
    
    SinglePlayerScenes
    -Include LevelSelect Script..
    - WinLevel() function can be called to transition to next level

---
+++ Assets/Scripts/LevelSelect/LevelSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+	//When Level won, transition PlayerPrefs to next level...
+	//Levels get unlock according. Level is automatically faded to new Level. 
+	public void WinLevel(){
+		int currentLevel = SceneManager.GetActiveScene ().buildIndex;
+		PlayerPrefs.SetInt ("LevelReached", currentLevel+1);
+	}
+	public void Reset(){
+		PlayerPrefs.SetInt ("LevelReached", 1);
+	}
+}
