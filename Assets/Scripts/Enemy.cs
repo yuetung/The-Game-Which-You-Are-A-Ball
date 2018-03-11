3f5a3ad1050eba4e6dc93a4f1757f03670193cd0
Yilong94 <yilong_tan@mymail.sutd.edu.sg>
2018-03-11 15:10:46 +0800

    created enemy health bar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
 	[Range(1,200)]
 	public int hp = 10;
+
+	public GameObject EnemyHealthBar;
+
+	public GameObject EnemyHealthBarPrefab;
+
+	//public GameObject canvas_parent;
 
 	[Range(0f,20f)]
 	public float moveSpeed = 3f;
@@ -19,6 +26,8 @@ public class Enemy : MonoBehaviour {
 	public bool loopWaypoints = true;
 
 	public GameObject explosionPrefab;
+
+	//public Transform RectBox;
 
 	[Tooltip("Set to true if the enemy can rotate when move")]
 	public bool canRotate = false;
@@ -38,6 +47,24 @@ public class Enemy : MonoBehaviour {
 		moveTime = 0f;
 		moving = true;
 	}
+
+	void CreateHealthBar(){
+		// instantiate a new enemyhealthbar gameobject from prefab
+//		GameObject UIHealthBar = Instantiate(EnemyHealthBarPrefab,new Vector2 (0,0),Quaternion.identity) as GameObject;
+		EnemyHealthBar = Instantiate(EnemyHealthBarPrefab,new Vector2 (0,0),Quaternion.identity) as GameObject;
+		// place this gameobject inside the canvas (only way to display a UI)
+		EnemyHealthBar.transform.SetParent(GameObject.FindGameObjectWithTag("MainCanvas").transform,false);
+		Debug.Log ("created enemy health bar");
+		// fill the enemyhealthbar with red color
+		Transform barFill = EnemyHealthBar.transform.Find ("Fill Area").transform.Find ("Fill");
+		barFill.GetComponent<Image>().color = Color.red;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("created");
+		CreateHealthBar ();
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -46,6 +73,16 @@ public class Enemy : MonoBehaviour {
 		} else {
 			_animator.SetBool ("Moving", false);
 		}
+
+		// update position of the health bar to follow the enemy
+		Vector3 TransformedPos = new Vector3 ((float)0, (float)0.7, (float)0);
+		Vector3 RectBoxPos = Camera.main.WorldToScreenPoint (this.transform.position + TransformedPos);
+		Debug.Log (RectBoxPos);
+		EnemyHealthBar.transform.position = RectBoxPos;
+
+		// update the health bar to show current health of enemy
+		float enemyhealth = hp/10.0f;
+		EnemyHealthBar.GetComponent<Slider> ().value = enemyhealth;
 	}
 
 	void EnemyMovement() {
@@ -97,6 +134,7 @@ public class Enemy : MonoBehaviour {
 				}
 				DestroyObject (transform.parent.gameObject);
 				DestroyObject (this.gameObject);
+				DestroyObject (EnemyHealthBar);
 			} else {
 				hp -= damage;
 			}
---
+++ Assets/Scripts/EnemyClamp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClamp : MonoBehaviour {
+
+	public Transform RectBox;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 RectBoxPos = Camera.main.WorldToScreenPoint (this.transform.position);
+		RectBox.transform.position = RectBoxPos;
+	}
+}
