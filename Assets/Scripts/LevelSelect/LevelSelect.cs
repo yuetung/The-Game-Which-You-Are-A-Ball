ec6d8e6312d17e5ea898e170966e2707fa4b5b78
KennySoh <32699647+KennySoh@users.noreply.github.com>
2018-03-12 22:39:35 +0800

    Sepearated Main menu & Level select (Menu/ ingame)
    Make it modular.

--- Assets/Scripts/LevelSelect/LevelSelect.cs
+++ Assets/Scripts/LevelSelect/LevelSelect.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour {
-
 	//When Level won, transition PlayerPrefs to next level...
 	//Levels get unlock according. Level is automatically faded to new Level. 
 	public void WinLevel(){
 		int currentLevel = SceneManager.GetActiveScene ().buildIndex;
 		PlayerPrefs.SetInt ("LevelReached", currentLevel+1);
-	}
-	public void Reset(){
-		PlayerPrefs.SetInt ("LevelReached", 1);
+		Debug.Log (PlayerPrefs.GetInt ("LevelReached"));
+		//NetworkManager_Custom nw = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManager_Custom>();
+		NetworkManager_Custom.StartSinglePlayer();
+		SceneManager.LoadScene(currentLevel+1);
 	}
 }
+
--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-	public Button level02Button, level03Button,level04Button;
     public Button startHost, joinGame;
-	int levelReached;
 
     void Start() {
         // assign starthost and joingame their onclicks
@@ -20,44 +18,21 @@ public class MainMenu : MonoBehaviour {
             startHost.onClick.AddListener(delegate { nw.StartupHost(); });
             joinGame.onClick.AddListener(delegate { nw.joinGame(); });
         }
-
-        levelReached = PlayerPrefs.GetInt ("LevelReached")-1;
-		level02Button.interactable = false;
-		level03Button.interactable = false;
-		level04Button.interactable = false;
-
-		switch (levelReached) {
-		case 2:
-			level02Button.interactable=true;
-			break;
-		case 3:
-			level02Button.interactable=true;
-			level03Button.interactable = true;
-			break;
-		case 4:
-			level02Button.interactable = false;
-			level03Button.interactable = false;
-			level04Button.interactable = false;
-			break;
-		}
-	}
-
-	public void levelToLoad(int level){
-		SceneManager.LoadScene (level);
+			
 	}
 
 	public void Play1(){
         NetworkManager_Custom.StartSinglePlayer();
-        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 2);
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 
 	}
 	public void Play2(){
-        Debug.Log(SceneManager.GetActiveScene().buildIndex);
-        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 3);
+		NetworkManager_Custom.StartSinglePlayer();
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 2);
         
 	}
 	public void BossAi(){
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 4);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 3);
 	}
 	public void Quit(){
 		Debug.Log ("Quit");
