9cdd26731de7a5a12ab130ab1234fdcc7cabe20c
Peng Hiang <33290649+penghiang@users.noreply.github.com>
2018-04-09 20:34:25 +0800

    pre-merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -361,7 +361,7 @@ public class PlayerController : NetworkBehaviour {
         {
             return;
         }
-        //Handheld.Vibrate();
+        Handheld.Vibrate();
         Debug.Log("Take damage");
         if (health - damage <= 0) {
             health = 0;
